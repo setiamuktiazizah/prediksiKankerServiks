6ce5ef2c02f651105c88fc5eebbf9d4c5d9ae334
Puditya Risky Amelia <pudityaskyml@gmail.com>
2023-06-23 12:39:09 +0700

    Benerin data gejala dan basis pengetahuan

--- database/migrations/2022_03_19_095534_create_tabel_data_gejala.php
+++ database/migrations/2022_03_19_095534_create_tabel_data_gejala.php
@@ -25,238 +25,238 @@ class CreateTabelDataGejala extends Migration
         $insertedData = [
             [
                 'kode_gejala' => 'G01',
-                'gejala' => 'Umur <= 12',
+                'gejala' => 'Usia kurang dari sama dengan 12 tahun',
                 // 'nilai_densitas' => 0.7,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'kode_gejala' => 'G02',
-                'gejala' => 'Umur 13-17',
+                'gejala' => 'Usia 13 sampai dengan 17 tahun',
                 // 'nilai_densitas' => 0.8,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'kode_gejala' => 'G03',
-                'gejala' => 'Umur 18-44',
+                'gejala' => 'Usia 18 sampai dengan 44 tahun',
                 // 'nilai_densitas' => 0.4,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'kode_gejala' => 'G04',
-                'gejala' => 'Umur 45-55',
+                'gejala' => 'Usia 45 sampai dengan 55 tahun',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'kode_gejala' => 'G05',
-                'gejala' => 'Umur 56-64',
+                'gejala' => 'Usia 56 sampai dengan 64 tahun',
                 // 'nilai_densitas' => 0.9,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'kode_gejala' => 'G06',
-                'gejala' => 'Umur >= 65',
+                'gejala' => 'Usia lebih dari sama dengan 65 tahun',
                 // 'nilai_densitas' => 0.3,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'kode_gejala' => 'G07',
-                'gejala' => 'Jumlah Partner Sex <= 2',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G08',
-                'gejala' => 'Jumlah partner sex 3',
-                // 'nilai_densitas' => 0.3,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G09',
-                'gejala' => 'Jumlah partner sex > 3',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G10',
-                'gejala' => 'Usia pertama kali berhubungan seksual <= 15',
-                // 'nilai_densitas' => 0.6,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G11',
-                'gejala' => 'Usia pertama kali berhubungan seksual 16-18',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G12',
-                'gejala' => 'Usia pertama kali berhubungan seksual > 18',
-                // 'nilai_densitas' => 0.8,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G13',
-                'gejala' => 'Jumlah kehamilan 0-1',
-                // 'nilai_densitas' => 0.8,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G14',
-                'gejala' => 'Jumlah kehamilan 2-3',
-                // 'nilai_densitas' => 0.3,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G15',
-                'gejala' => 'Jumlah kehamilan > 3',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G16',
-                'gejala' => 'Lama merokok (tahun) 0-1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G17',
-                'gejala' => 'Lama merokok (tahun) > 1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G18',
-                'gejala' => 'Jumlah rokok yang dihabiskan per tahun (pack) 0-1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G19',
-                'gejala' => 'Jumlah rokok yang dihabiskan per tahun (pack) > 1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G20',
-                'gejala' => 'Penggunaan Hormonal Contraceptive (per tahun) 0-1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G21',
-                'gejala' => 'Penggunaan Hormonal Contraceptive (per tahun) 2-3',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G22',
-                'gejala' => 'Penggunaan Hormonal Contraceptive (per tahun) > 3',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G23',
-                'gejala' => 'Penggunaan IUD (per tahun) 0-2',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G24',
-                'gejala' => 'Penggunaan IUD (per tahun) > 2',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G25',
-                'gejala' => 'STDs number 0-1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G26',
-                'gejala' => 'STDs number > 1',
-                // 'nilai_densitas' => 0.5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            [
-                'kode_gejala' => 'G27',
                 'gejala' => 'Merokok',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G28',
+                'kode_gejala' => 'G08',
                 'gejala' => 'Tidak merokok',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G29',
+                'kode_gejala' => 'G09',
+                'gejala' => 'Lama merokok 0 sampai dengan 1 tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G10',
+                'gejala' => 'Lama merokok lebih dari 1 tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G11',
+                'gejala' => 'Jumlah rokok yang dihabiskan per tahun 0 sampai dengan 1 pack',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G12',
+                'gejala' => 'Jumlah rokok yang dihabiskan per tahun lebih dari 1 pack',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G13',
                 'gejala' => 'Pernah berhubungan seksual',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G30',
+                'kode_gejala' => 'G14',
                 'gejala' => 'Tidak pernah berhubungan seksual',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G31',
+                'kode_gejala' => 'G15',
+                'gejala' => 'Usia pertama kali berhubungan seksual kurang dari sama dengan 15 tahun',
+                // 'nilai_densitas' => 0.6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G16',
+                'gejala' => 'Usia pertama kali berhubungan seksual 16 samapi dengan 18 tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G17',
+                'gejala' => 'Usia pertama kali berhubungan seksual lebih dari 18 tahun',
+                // 'nilai_densitas' => 0.8,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G18',
+                'gejala' => 'Jumlah Partner Sex kurang dari sama dengan 2',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G19',
+                'gejala' => 'Jumlah partner sex ada 3',
+                // 'nilai_densitas' => 0.3,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G20',
+                'gejala' => 'Jumlah partner sex lebih dari 3',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G21',
                 'gejala' => 'Menggunakan konstraseptif hormonal',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G32',
+                'kode_gejala' => 'G22',
                 'gejala' => 'Tidak menggunakan konstraseptif hormonal',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G33',
+                'kode_gejala' => 'G23',
+                'gejala' => 'Penggunaan Hormonal Contraceptive 0 sampai dengan 1 kali per tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G24',
+                'gejala' => 'Penggunaan Hormonal Contraceptive 2 sampai dengan 3 kali per tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G25',
+                'gejala' => 'Penggunaan Hormonal Contraceptive lebih dari 3 kali per tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G26',
+                'gejala' => 'Jumlah kehamilan 0 sampai 1 kali',
+                // 'nilai_densitas' => 0.8,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G27',
+                'gejala' => 'Jumlah kehamilan 2 sampai 3 kali',
+                // 'nilai_densitas' => 0.3,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G28',
+                'gejala' => 'Jumlah kehamilan lebih dari 3 kali',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G29',
                 'gejala' => 'Menggunakan IUD',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'kode_gejala' => 'G34',
+                'kode_gejala' => 'G30',
                 'gejala' => 'Tidak menggunakan IUD',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G31',
+                'gejala' => 'Penggunaan IUD 0 sampai dengan 2 kali per tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G32',
+                'gejala' => 'Penggunaan IUD lebih dari 2 kali per tahun',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G33',
+                'gejala' => 'Terinfeksi STDs sebanyak 0 sampai dengan 1 kali',
+                // 'nilai_densitas' => 0.5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'kode_gejala' => 'G34',
+                'gejala' => 'Terinfeksi STDs sebanyak lebih dari 1 kali',
                 // 'nilai_densitas' => 0.5,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
